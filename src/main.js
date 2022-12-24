9712ba1665619636f31e1074c90e6add9f79707a
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-12-24 00:39:52 -0800

    setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,53 @@
 // For more information, see https://crawlee.dev/
-import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
-import { router } from './routes.js';
+import { CheerioCrawler, Dataset, log } from "crawlee";
+import { router } from "./routes.js";
 
-const startUrls = ['https://crawlee.dev'];
+let enqueuedCount = 0;
+let processedCount = 0;
+let errorCount = 0;
+
+const startUrls = [];
+
+// Generate URLs for creator profiles
+const creatorBaseURL = "https://app.hellothematic.com/creator/profile/";
+const numCreatorProfiles = 100; // Number of creator profiles to generate URLs for
+const creatorProfiles = []; // Array to hold the URLs
+
+for (let i = 1; i <= numCreatorProfiles; i++) {
+  const url = creatorBaseURL + i; // Construct the URL for the current creator profile
+  creatorProfiles.push(url); // Add the URL to the array
+}
+
+console.log(creatorProfiles); // Output the array of URLs
+
+// Generate URLs for artist profiles
+const artistBaseURL = "https://app.hellothematic.com/artist/profile/";
+const numArtistProfiles = 100; // Number of artist profiles to generate URLs for
+const artistProfiles = []; // Array to hold the URLs
+
+for (let i = 1; i <= numArtistProfiles; i++) {
+  const url = artistBaseURL + i; // Construct the URL for the current artist profile
+  artistProfiles.push(url); // Add the URL to the array
+}
+
+console.log(artistProfiles); // Output the array of URLs
+
+// Combine the URLs for creator and artist profiles
+const allProfiles = [...creatorProfiles, ...artistProfiles];
+
+// Add the URLs to the startUrls array
+startUrls.push(...allProfiles);
 
 const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+  useSessionPool: true,
 });
 
 await crawler.run(startUrls);
+
+log.info(`Enqueued: ${enqueuedCount} URLs`);
+log.info(`Processed: ${processedCount} URLs`);
+log.info(`Errors: ${errorCount} URLs`);
+
+//export the dataset to a single JSON file
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,75 @@
-import { Dataset, createCheerioRouter } from 'crawlee';
+import { Dataset, createCheerioRouter, utils } from "crawlee";
 
 export const router = createCheerioRouter();
 
+let enqueuedCount = 0;
+let processedCount = 0;
+let errorCount = 0;
+
 router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
-    await enqueueLinks({
-        globs: ['https://crawlee.dev/**'],
-        label: 'detail',
-    });
+  const currentTime = new Date().toString();
+  log.info(`[${currentTime}] enqueueing new URLs`);
+  await enqueueLinks({
+    globs: ["https://app.hellothematic.com/artist/profile/**"],
+    label: "artist",
+  });
+  await enqueueLinks({
+    globs: ["https://app.hellothematic.com/creator/profile/**"],
+    label: "creator",
+  });
+  enqueuedCount += 1;
 });
 
-router.addHandler('detail', async ({ request, $, log }) => {
-    const title = $('title').text();
-    log.info(`${title}`, { url: request.loadedUrl });
+router.addHandler("artist", async ({ request, $, log }) => {
+  const currentTime = new Date().toString();
+  try {
+    const title = $("title").text();
+    log.info(`[${currentTime}] ${title} - ${request.loadedUrl}`);
+    const name = $("div.name-wrapper h1").text();
+    const imgSrc = $("img.profile-image-round").attr("src");
+    const bio = $(".person-description").text();
+    const socialLinks = utils.social.parseHandlesFromHtml($("ul.social-media-links").html());
+    const profileType = "artist";
 
     await Dataset.pushData({
-        url: request.loadedUrl,
-        title,
+      url: request.loadedUrl,
+      title,
+      name,
+      bio,
+      imgSrc,
+      socialLinks,
+      profileType,
     });
+    processedCount += 1;
+  } catch (error) {
+    log.error(`[${currentTime}] Error processing URL: ${error.message}`);
+    errorCount += 1;
+  }
+});
+
+router.addHandler("creator", async ({ request, $, log }) => {
+  const currentTime = new Date().toString();
+  try {
+    const title = $("title").text();
+    log.info(`[${currentTime}] ${title} - ${request.loadedUrl}`);
+    const name = $("div.creator-name-wrapper h1").text();
+    const imgSrc = $("img.creator-profile-image-round").attr("src");
+    const bio = $(".creator-bio").text();
+    const socialLinks = utils.social.parseHandlesFromHtml($("ul.creator-social-media-links").html());
+    const profileType = "creator";
+
+    await Dataset.pushData({
+      url: request.loadedUrl,
+      title,
+      name,
+      imgSrc,
+      bio,
+      socialLinks,
+      profileType,
+    });
+    processedCount += 1;
+  } catch (error) {
+    log.error(`[${currentTime}] Error processing URL: ${error.message}`);
+    errorCount += 1;
+  }
 });
